abef11a3b5028d904527504447c3cc02a26d85d0
Ash <148392742+ashilated@users.noreply.github.com>
2025-11-08 12:31:48 -0800

    get metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
-'use client'
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
 import analyzeImage from "@/functions/ImageAnalyzer";
 
 export default async function Home() {
+    console.log("running function")
+    await analyzeImage("/home/ashilated/Documents/IPTC-GoogleImgSrcPmd_testimg01.jpg")
 
-
-    function runTest() {
-        analyzeImage("/home/ashilated/Documents/IPTC-GoogleImgSrcPmd_testimg01.jpg")
-    }
 
     return (
         <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <Button><Link href="/login">Log In</Link></Button>
             <Button><Link href="/signup">Sign Up</Link></Button>
 
-            <Button onClick={runTest}>test</Button>
         </div>
     );
 }
--- functions/ImageAnalyzer.tsx
+++ functions/ImageAnalyzer.tsx
@@ -1,14 +1,6 @@
-export default async function analyzeImage(imageUrl: string) {
-    fetch(imageUrl)
-        .then(res => res.blob())
-        .then(blob => {
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                console.log(reader.result);
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-expect-error
-                const base64 = reader.result.replace('data:', '').replace(/^.+,/, '');
-            }
-            reader.readAsDataURL(blob)
-        })
+import exifr from 'exifr'
+
+export default async function ImageAnalyzer() {
+    await exifr.parse('https://github.com/ianare/exif-samples/blob/master/jpg/Canon_40D_photoshop_import.jpg?raw=true')
+        .then(output => console.log(output))
 }
