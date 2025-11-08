90e7afddb75c7566f192e37be0450a09bdf12842
Ash <148392742+ashilated@users.noreply.github.com>
2025-11-08 12:40:57 -0800

    catch error

--- functions/ImageAnalyzer.tsx
+++ functions/ImageAnalyzer.tsx
@@ -1,6 +1,10 @@
 import exifr from 'exifr'
 
 export default async function ImageAnalyzer() {
-    await exifr.parse('https://github.com/ianare/exif-samples/blob/master/jpg/Canon_40D_photoshop_import.jpg?raw=true')
-        .then(output => console.log(output))
+    try {
+        const {latitude, longitude} = await exifr.gps('https://github.com/ianare/exif-samples/blob/master/jpg/Canon_40D_photoshop_import.jpg?raw=true')
+        console.log(latitude, longitude)
+    } catch (error) {
+        console.log("no metadata for this image")
+    }
 }
