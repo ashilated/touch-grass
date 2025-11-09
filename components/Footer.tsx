6eeede59911060ecfae048951d5689e18229bf56
toashid-tech <toashid@gmail.com>
2025-11-09 11:03:27 -0800

    footer

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer
+            className="w-full h-32 bg-[url('/grass.png')] bg-repeat-x bg-bottom"
+            style={{
+                backgroundSize: "contain",
+                imageRendering: "pixelated",
+            }}
+        />
+    );
+}
